586056db0b258c27421578480bd1c4d1969512f7
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2025-01-11 12:06:39 +0530

    mogo conected

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import dotenv  from "dotenv";
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -10,8 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=> res.send("Hellow")
-    
-)
+app.get("/", (req, res) => res.send("Hellow"));
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("Error connecting to MongoDB:", err));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
